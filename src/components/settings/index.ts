27a9ccefaf7de4268e12ba0ed8d209a492947130
rafekyyy-beep <rafekyyy@gmail.com>
2026-06-05 18:57:29 +0300

    Create index.ts

---
+++ src/components/settings/index.ts
@@ -0,0 +1,4 @@
+export { ProfileSettings } from './ProfileSettings';
+export { NotificationSettings } from './NotificationSettings';
+export { ThemeSettings } from './ThemeSettings';
+export { SecuritySettings } from './SecuritySettings';
