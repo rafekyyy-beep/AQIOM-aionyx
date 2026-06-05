495665581a58e149c53b0aab06241a56a40574c6
rafekyyy-beep <rafekyyy@gmail.com>
2026-06-05 04:16:57 +0300

    Create index.ts

---
+++ src/components/chat/index.ts
@@ -0,0 +1,4 @@
+export { ChatView } from './ChatView';
+export { MessageBubble } from './MessageBubble';
+export { ChatInput } from './ChatInput';
+export { TypingIndicator } from './TypingIndicator';
