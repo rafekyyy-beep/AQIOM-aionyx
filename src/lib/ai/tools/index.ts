a2c4582692e7243d5514bfb23e18dcdab2620cb7
rafekyyy-beep <rafekyyy@gmail.com>
2026-06-06 17:52:11 +0300

    Create index.ts

---
+++ src/lib/ai/tools/index.ts
@@ -0,0 +1,5 @@
+export { webSearch, formatSearchResults } from './web-search';
+export { calculate, calculatorTool } from './calculator';
+export { getCurrentDateTime, getDate, getTime, getYear } from './datetime';
+export { analyzeText, extractKeywords } from './text-analyzer';
+export { logicalReasoner } from './logical-reasoner';
